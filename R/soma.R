e5b8ccc5d78c4b2e24c43da0ad50e39910fe2d5c
Debora <deborafarias297@gmail.com>
2021-09-02 16:15:01 -0300

    Initial commit

---
+++ R/soma.R
@@ -0,0 +1,21 @@
+#' Addition
+#'
+#' In addition there is the calculus of adding natural numbers to others.
+#' This mathematical operation is also popularly known as addition. The
+#' final result of the addition is called the total or sum and the numbers used are
+#' the parcels. The arithmetic operator, that is, the sign that indicates
+#'  its calculation is the (+)
+#'
+#' @param a number
+#' @param b number
+#'
+#' @return number
+#' @export
+#'
+#' @examples soma(9,2)
+
+
+
+soma<-function(a,b){
+  a+b
+}
